e3efbbf9668776d52a993814677b4b05eadc10c3
GabrielPaiva <gabrielanpaiva2005@gmail.com>
2024-05-02 22:07:37 -0300

    modularizacao e fundo stalker

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="app">
+      <!-- Seu componente de player de vídeo -->
+      <VideoPlayer/>
+  
+      <!-- Conteúdo adicional aqui -->
+  
+    </div>
+  </template>
+  
+  <script>
+  // Importe seu componente de player de vídeo
+  import VideoPlayer from './components/VideoPlayer.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      VideoPlayer
+    }
+  }
+  </script>
+  
+  <style>
+  /* Remova as margens padrão do corpo do documento */
+  body {
+    margin: 0;
+    /* Defina a imagem de fundo */
+    background-image: url('https://burningthecelluloid.files.wordpress.com/2020/08/2025407.jpg?w=1024');
+    /* Defina o tamanho e posicionamento do background */
+    background-size: cover;
+    background-position: center;
+  }
+  
+</style>
+  
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import Home from './VideoPlayer.vue'
+import Home from './App.vue'
 
 createApp(Home).mount('#app')
